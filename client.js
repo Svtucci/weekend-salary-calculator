1b60f78b4115dae4d29e9fd81a3c6f0b6609c249
Stephen Vertucci <steve.vertucci@gmail.com>
2023-02-19 16:13:02 -0500

    added early functionality to data, making sure querysleector is pulling correctly

--- client.js
+++ client.js
@@ -1,1 +1,17 @@
 console.log('Hello World')
+
+function submitForm(event) {
+    console.log('submitForm');
+    event.preventDefault();
+    let firstName = document.querySelector('#first-input').value;
+    let lastName = document.querySelector('#last-input').value;
+    let id = document.querySelector('#id-input').value;
+    let title = document.querySelector('#title-input').value;
+    let annualSalary = document.querySelector('#annual-input').value;
+    console.log(firstName);
+    console.log(lastName);
+    console.log(id);
+    console.log(title);
+    console.log(annualSalary);
+
+}
